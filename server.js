8061f7bbcb2727037715fe1be2db09a06bfb9205
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-04-09 19:42:20 +0200

    create app.js to start application from it and make server a Server Module

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require('http');
 
-function onRequest(request, response) {
-    console.log('Request Received');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello from out application');
-    response.end();
+function startServer() {
+    function onRequest(request, response) {
+        console.log('Request Received');
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello from our server module');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8000);
+    console.log('Server start on localhost port 8000');
 }
 
-http.createServer(onRequest).listen(8000);
-console.log('Server start on localhost port 8000');
+exports.startServer = startServer;
